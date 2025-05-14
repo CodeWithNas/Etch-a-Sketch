59ae524d9684064a395634ced1e35860d728b939
CodeWithNas <183351791+CodeWithNas@users.noreply.github.com>
2025-05-14 10:30:08 +0200

    Improve JavaScript code for better User Experience

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+let isDrawing = false;
+
 function createGrid(size) {
     const container = document.getElementById("container");
     container.innerHTML = "";
 
     const squareSize = 960 / size;
 
-    for (i = 0; i < size * size; i++) {
+    container.addEventListener('mousedown', () => {
+      isDrawing = true;
+    });
+
+    container.addEventListener('mouseup', () => {
+      isDrawing = false;
+    });
+
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("grid-square");
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         square.addEventListener('mouseenter', () => {
+          if (isDrawing) {
             square.style.backgroundColor = 'black';
-        });
+          }
+        });    
+        
 
         container.appendChild(square);
     }
 }
 
-createGrid(16);
 
-
-document.getElementById('resizeBtn').addEventListener('click', () => {
+ document.getElementById('resizeBtn').addEventListener('click', () => {
     let newSize = parseInt(prompt('Enter new grid size (1 to 100):'));
   
     if (newSize && newSize > 0 && newSize <= 100) {
@@ -29,4 +40,6 @@ document.getElementById('resizeBtn').addEventListener('click', () => {
       alert("Please enter a number between 1 and 100.");
     }
   });
-  
+
+
+  createGrid(16);
